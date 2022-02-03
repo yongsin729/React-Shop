bd6f389bed2a0558fb9f0695d34bfd8de9f16fe9
yunghsin Lee <lenalee7963@gmail.com>
2022-02-03 20:57:50 +0900

    first commit2

--- src/Detail.js
+++ src/Detail.js
@@ -1,7 +1,19 @@
-import React,{useState} from 'react';
+import React,{useEffect, useState} from 'react';
 import { useHistory,useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import styled from 'styled-components';
+import './Detail.scss';
+
+let box1=styled.div`
+    padding:20px
+`;
+let 제목=styled.h4`
+    font-size:25px;
+`;
 
 function Detail(props){
+
+    useEffect
+
     let {id}=useParams();
     let 찾은상품 = props.shoes.find(function(상품){
         return 상품.id==id
@@ -10,6 +22,13 @@ function Detail(props){
     let history=useHistory();
     return (
         <div className="container">
+            <box1>
+                <제목 className='red'>Detail</제목>
+            </box1>
+            <div className='my-alert-yellow'>
+                <p>재고가 얼마 남지 않았습니다</p>
+            </div>
+
       <div className="row">
         <div className="col-md-6">
           <img src={"https://codingapple1.github.io/shop/shoes"+(찾은상품.id+1)+".jpg"} width="100%" />
